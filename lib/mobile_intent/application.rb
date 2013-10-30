edc4a690e817f71598798f86a77be4a5381f90e2
masarakki <masaki@hisme.net>
2013-10-31 03:56:31 +0900

    add market url

--- lib/mobile_intent/application.rb
+++ lib/mobile_intent/application.rb
@@ -11,5 +11,23 @@ module MobileIntent
     def ios? ; !!@ios ; end
     def android? ; !!@android ; end
 
+    def android_intent_url(url)
+      raise unless android?
+      "intent://#{url}#Intent;scheme=#{name};package=#{@android};end"
+    end
+
+    def android_market_url
+      raise unless android?
+      "market://details?id=#{@android}"
+    end
+
+    def ios_market_url
+      raise unless ios?
+      "itms://itunes.com/apps/#{@ios}"
+    end
+
+    def intent_url(url)
+      "#{name}://#{url}"
+    end
   end
 end
--- spec/mobile_intent/application_spec.rb
+++ spec/mobile_intent/application_spec.rb
@@ -1,18 +1,41 @@
 require 'spec_helper'
 
 describe MobileIntent::Application do
+  let(:name) { :app_name }
+  let(:android) { 'com.example.myapp' }
+  let(:ios) { 'myappname' }
   subject { application }
 
   context :with_android do
-    let(:application) { described_class.new :app_name, :android => 'com.example.myapp' }
+    let(:application) { described_class.new name, android: android }
     it { expect(subject).to be_android }
     it { expect(subject).not_to be_ios }
+    describe :intent_url do
+      it { expect(subject.intent_url(:hoge)).to eq "#{name}://hoge" }
+    end
+    its(:android_market_url) { should eq "market://details?id=#{android}" }
+    describe :android_intent_url do
+      it { expect(subject.android_intent_url(:hoge)).to eq "intent://hoge#Intent;scheme=#{name};package=#{android};end" }
+    end
+    describe :ios_market_url do
+      it { expect { subejct.ios_market_url }.to raise_error }
+    end
   end
 
   context :with_ios do
-    let(:application) { described_class.new :app_name, :ios => 'myappname' }
+    let(:application) { described_class.new name, ios: ios }
     it { expect(subject).to be_ios }
     it { expect(subject).not_to be_android }
+    describe :intent_url do
+      it { expect(subject.intent_url(:hoge)).to eq "#{name}://hoge" }
+    end
+    its(:ios_market_url) { should eq "itms://itunes.com/apps/#{ios}" }
+    describe :android_market_url do
+      it { expect { subject.android_market_url }.to raise_error }
+    end
+    describe :android_intent_url do
+      it { expect { subject.android_intent_url(:hoge) }.to raise_error }
+    end
   end
 
 end
